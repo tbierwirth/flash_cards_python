987a476d91166e3c96a4c214f30564f036ca78fe
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-01-27 12:59:24 -0700

    Create test for card class and attributes

---
+++ tests/test_card.py
@@ -0,0 +1,16 @@
+import unittest
+from lib.card import Card
+
+class CardClass(unittest.TestCase):
+    def test_it_exists(self):
+        card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        self.assertIsInstance(card, Card)
+
+    def test_attributes(self):
+        card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        self.assertEqual(card.question, "What is the capital of Alaska?")
+        self.assertEqual(card.answer, "Juneau")
+        self.assertEqual(card.category, "Geography")
+
+if __name__ == '__main__':
+    unittest.main()
