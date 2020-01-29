1cd75703b1c8dec779129b87fb26a0bbb64f1336
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-01-29 15:21:43 -0700

    Fix syntax of method being called

--- tests/test_turn.py
+++ tests/test_turn.py
@@ -7,3 +7,22 @@ class TurnClass(unittest.TestCase):
         card = Card("What is the capital of Alaska?", "Juneau", "Geography")
         turn = Turn("Juneau", card)
         self.assertIsInstance(turn, Turn)
+
+    def test_correct_guess(self):
+        card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        turn = Turn("Juneau", card)
+        self.assertEqual(turn.card, card)
+        self.assertEqual(turn.guess, "Juneau")
+        self.assertTrue(turn.is_correct())
+        self.assertEqual(turn.feedback(), "Correct!")
+
+    def test_incorrect_guess(self):
+        card = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        turn = Turn("Topeka", card)
+        self.assertEqual(turn.card, card)
+        self.assertEqual(turn.guess, "Topeka")
+        self.assertFalse(turn.is_correct())
+        self.assertEqual(turn.feedback(), "Incorrect")
+
+if __name__ == '__main__':
+    unittest.main()
