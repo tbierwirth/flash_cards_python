ad8c6b8f3487f602193997d1a194ee7d8ac847d8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-05 17:36:38 -0700

    Create test for Round class and basic assertions

---
+++ tests/test_round.py
@@ -0,0 +1,15 @@
+import unittest
+from lib.card import Card
+from lib.deck import Deck
+from lib.round import Round
+
+class RoundClass(unittest.TestCase):
+    def test_it_exists(self):
+        card_1 = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        card_2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
+        card_3 = Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM")
+        cards = [card_1, card_2, card_3]
+        deck = Deck(cards)
+        round = Round(deck)
+        self.assertIsInstance(round, Round)
+        self.assertEqual(round.deck, deck)
