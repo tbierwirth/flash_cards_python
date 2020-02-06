fcece3db3b3824622550d2854b41896763b6c277
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-05 17:38:47 -0700

    Add test for current_card function

--- tests/test_round.py
+++ tests/test_round.py
@@ -13,3 +13,12 @@ class RoundClass(unittest.TestCase):
         round = Round(deck)
         self.assertIsInstance(round, Round)
         self.assertEqual(round.deck, deck)
+
+    def test_current_card(self):
+        card_1 = Card("What is the capital of Alaska?", "Juneau", "Geography")
+        card_2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
+        card_3 = Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM")
+        cards = [card_1, card_2, card_3]
+        deck = Deck(cards)
+        round = Round(deck)
+        self.assertEqual(round.current_card(), card_1)
