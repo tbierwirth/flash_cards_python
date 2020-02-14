c862dc608dcc214dde66542d209c672c688aa244
Tyler Bierwirth <tylerbierwirth@gmail.com>
2020-02-14 12:31:14 -0700

    Add percent correct by category breakdown to runner

--- flashcards.py
+++ flashcards.py
@@ -7,6 +7,7 @@ card_1 = Card("What is the capital of Alaska?", "Juneau", "Geography")
 card_2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
 card_3 = Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM")
 cards = [card_1, card_2, card_3]
+categories = []
 
 deck = Deck(cards)
 round = Round(deck)
@@ -16,6 +17,9 @@ print("-------------------------------------")
 
 i = 1
 for card in cards:
+    if card.category not in categories:
+        categories.append(card.category)
+
     print(f"This is card number {i} out of {deck.count()}")
     print(f"Question: {card.question}")
     guess = input("")
@@ -25,3 +29,5 @@ for card in cards:
 
 print("****** Game over! ******")
 print(f"You had {round.number_correct} out of {deck.count()} for a total score of {round.percent_correct()}%.")
+for category in categories:
+    print(f"{category} - {round.percent_correct_by_category(category)}% correct")
